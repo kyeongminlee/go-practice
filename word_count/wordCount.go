633f1cc29977c9ff340db0c22767f66527c463eb
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2024-02-05 00:07:51 +0900

    feature - word count 기능 구현

--- word_count/wordCount.go
+++ word_count/wordCount.go
@@ -1,5 +1,14 @@
 package main
 
-func WordCount() {
+import "strings"
 
+func WordCount(sentence string) map[string]int {
+	words := strings.Split(sentence, " ")
+	wordMap := make(map[string]int)
+
+	for _, word := range words {
+		wordMap[word]++
+	}
+
+	return wordMap
 }
