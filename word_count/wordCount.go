baf9102d71bfaac0d6bf9aa5615d8d3052f4180a
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2024-02-05 00:11:27 +0900

    refactor - goroutine으로 수정

--- word_count/wordCount.go
+++ word_count/wordCount.go
@@ -1,14 +1,31 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"sync"
+)
+
+var (
+	mutex sync.Mutex
+	wg    sync.WaitGroup
+)
 
 func WordCount(sentence string) map[string]int {
 	words := strings.Split(sentence, " ")
 	wordMap := make(map[string]int)
 
 	for _, word := range words {
-		wordMap[word]++
+		wg.Add(1)
+		go func(word string) {
+			defer wg.Done()
+
+			mutex.Lock()
+			defer mutex.Unlock()
+			wordMap[word]++
+		}(word)
 	}
+
+	wg.Wait()
 
 	return wordMap
 }
