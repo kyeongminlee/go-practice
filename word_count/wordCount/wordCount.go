c445a9658c642f6a1177f02aacae3d8154f710d7
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2024-02-05 23:29:01 +0900

    refactor - mutax 대신 channel 사용

--- word_count/wordCount/wordCount.go
+++ word_count/wordCount/wordCount.go
@@ -14,18 +14,25 @@ func WordCount(sentence string) map[string]int {
 	words := strings.Split(sentence, " ")
 	wordMap := make(map[string]int)
 
-	for _, word := range words {
-		wg.Add(1)
-		go func(word string) {
-			defer wg.Done()
+	ch := make(chan string)
 
-			mutex.Lock()
-			defer mutex.Unlock()
-			wordMap[word]++
-		}(word)
+	go func() {
+		defer close(ch)
+
+		for _, word := range words {
+			wg.Add(1)
+			go func(word string) {
+				defer wg.Done()
+				ch <- word
+			}(word)
+		}
+
+		wg.Wait()
+	}()
+
+	for word := range ch {
+		wordMap[word]++
 	}
-
-	wg.Wait()
 
 	return wordMap
 }
